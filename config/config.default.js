581d65a02299ea10d82156a9cec03474107b064f
sx807 <sxing.00789@gmail.com>
2019-12-31 14:28:14 +0800

    update 20192131

--- config/config.default.js
+++ config/config.default.js
@@ -25,7 +25,9 @@ module.exports = appInfo => {
       // 单数据库信息配置
       client: {
         // host
-        host: 'localhost',
+        // host: 'localhost',
+        host: '192.168.1.191',
+        // host: 'localhost',
         // 端口号
         port: '3306',
         // 用户名
